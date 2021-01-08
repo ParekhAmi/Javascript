c192c219cfbcbc0b45ad90d0ad04278d87d535ae
ParekhAmi <amiparekh87@gmail.com>
2021-01-08 03:20:23 -0500

    Projects folder added

---
+++ Projects/calculator.js
@@ -0,0 +1,35 @@
+function numdisonscreen(num) {
+
+    calc.screen.value = num;
+    
+
+}
+
+
+
+function Arithmetic(opr) {
+    var a = calc.n1.value;
+    var b = calc.n2.value;
+    var c;
+    if (opr == "+") {
+        c = parseInt(a) + parseInt(b);
+
+    }
+    if (opr == "-") {
+        c = parseInt(a) - parseInt(b);
+
+    }
+    if (opr == "*") {
+        c = parseInt(a) * parseInt(b);
+
+    }
+    if (opr == "/") {
+        c = parseInt(a) / parseInt(b);
+
+    }
+    if (opr == "%") {
+        c = parseInt(a) % parseInt(b);
+
+    }
+    calc.ans.value = c;
+}
